ccafef71d93dfdc4f6d4e0cce9ef02309676fc3a
Narunart Tuntue <narunart.t@arise.tech>
2024-06-30 19:46:21 +0700

    [1] should return 15 when input is XV

--- main_test.go
+++ main_test.go
@@ -103,4 +103,18 @@ func TestRomanToInt(t *testing.T) {
 			t.Errorf("want %v , but got %v", want, got)
 		}
 	})
+
+	t.Run("should return 15 when input is XV", func(t *testing.T) {
+		// Arrange
+		input := "XV"
+		want := 15
+
+		// Act
+		got := romanToInt(input)
+
+		// Assert
+		if got != want {
+			t.Errorf("want %v , but got %v", want, got)
+		}
+	})
 }
