af32c842d424935e16190f42f092efc988a1ca4e
Narunart Tuntue <narunart.t@arise.tech>
2024-06-30 22:15:00 +0700

    [3] should return 1 when input is I

--- main.go
+++ main.go
@@ -1,11 +1,7 @@
 package main
 
 func romanToInt(s string) int {
-<<<<<<< HEAD
 	romanMap := map[byte]int{
-=======
-	romanMap := map[byte] int {
->>>>>>> 297b62f ([3] should return 1 when input is I)
 		'I' : 1,
 		'V' : 5,
 		'X' : 10,
@@ -16,17 +12,14 @@ func romanToInt(s string) int {
 	n := len(s)
 	total := romanMap[s[n-1]]
 
-<<<<<<< HEAD
-	for i := n-2 ; i >= 0 ; i-- {
-=======
 	for i := n - 2 ; i >= 0 ; i-- {
->>>>>>> 297b62f ([3] should return 1 when input is I)
 		if romanMap[s[i]] < romanMap[s[i+1]] {
 			total -= romanMap[s[i]]
-		} else {
+		}else {
 			total += romanMap[s[i]]
 		}
 	}
 
 	return total
+
 }
