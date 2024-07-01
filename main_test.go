6bd0561ab7eaba90050746d01ada29032fa32454
Narunart Tuntue <narunart.t@arise.tech>
2024-07-02 00:43:11 +0700

    [2] should return 9 when input is IX

--- main_test.go
+++ main_test.go
@@ -61,4 +61,18 @@ func TestRomanToInt(t *testing.T) {
 		}
 	})
 
+	t.Run("should return 9 when input is IX", func(t *testing.T) {
+		// Arrange
+		input := "IX"
+		want := 9
+
+		// Act
+		got := romanToInt(input)
+
+		// Assert
+		if got != want {
+			t.Errorf("want %v , but got %v", want, got)
+		}
+	})
+
 }
