a5225360cf9ed866904282a38b6f8c2693d97bf5
Narunart Tuntue <narunart.t@arise.tech>
2024-07-02 00:42:25 +0700

    [2] should return 5 when input is V

--- main_test.go
+++ main_test.go
@@ -47,4 +47,18 @@ func TestRomanToInt(t *testing.T) {
 		}
 	})
 
+	t.Run("should return 5 when input is V", func(t *testing.T) {
+		// Arrange
+		input := "V"
+		want := 5
+
+		// Act
+		got := romanToInt(input)
+
+		// Assert
+		if got != want {
+			t.Errorf("want %v , but got %v", want, got)
+		}
+	})
+
 }
