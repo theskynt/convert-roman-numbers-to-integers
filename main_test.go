99bf6222399d2f45b7a452d39ba163a6f5f34a69
Narunart Tuntue <narunart.t@arise.tech>
2024-07-02 00:53:07 +0700

    [3] should return 4 when input is IV

--- main_test.go
+++ main_test.go
@@ -33,19 +33,19 @@ func TestRomanToInt(t *testing.T) {
 		}
 	})
 
-	// t.Run("should return 4 when input is IV", func(t *testing.T) {
-	// 	// Arrange
-	// 	input := "IV"
-	// 	want := 4
+	t.Run("should return 4 when input is IV", func(t *testing.T) {
+		// Arrange
+		input := "IV"
+		want := 4
 
-	// 	// Act
-	// 	got := romanToInt(input)
+		// Act
+		got := romanToInt(input)
 
-	// 	// Assert
-	// 	if got != want {
-	// 		t.Errorf("want %v , but got %v", want, got)
-	// 	}
-	// })
+		// Assert
+		if got != want {
+			t.Errorf("want %v , but got %v", want, got)
+		}
+	})
 
 	// t.Run("should return 5 when input is V", func(t *testing.T) {
 	// 	// Arrange
