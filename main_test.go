240f8dcc007e6229f32e35aad97ddbd89e50d14b
Narunart Tuntue <narunart.t@arise.tech>
2024-07-02 00:52:48 +0700

    [3] should return 3 when input is III

--- main_test.go
+++ main_test.go
@@ -19,19 +19,19 @@ func TestRomanToInt(t *testing.T) {
 		}
 	})
 
-	// t.Run("should return 3 when input is III", func(t *testing.T) {
-	// 	// Arrange
-	// 	input := "III"
-	// 	want := 3
+	t.Run("should return 3 when input is III", func(t *testing.T) {
+		// Arrange
+		input := "III"
+		want := 3
 
-	// 	// Act
-	// 	got := romanToInt(input)
+		// Act
+		got := romanToInt(input)
 
-	// 	// Assert
-	// 	if got != want {
-	// 		t.Errorf("want %v , but got %v", want, got)
-	// 	}
-	// })
+		// Assert
+		if got != want {
+			t.Errorf("want %v , but got %v", want, got)
+		}
+	})
 
 	// t.Run("should return 4 when input is IV", func(t *testing.T) {
 	// 	// Arrange
