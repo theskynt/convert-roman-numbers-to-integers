9aa0295dd9c61f494b04bfb8481ae165505a531a
Narunart Tuntue <narunart.t@arise.tech>
2024-07-02 00:54:38 +0700

    [3] should return 19 when input is XIX

--- main_test.go
+++ main_test.go
@@ -103,19 +103,19 @@ func TestRomanToInt(t *testing.T) {
 		}
 	})
 
-	// t.Run("should return 19 when input is XIX", func(t *testing.T) {
-	// 	// Arrange
-	// 	input := "XIX"
-	// 	want := 19
+	t.Run("should return 19 when input is XIX", func(t *testing.T) {
+		// Arrange
+		input := "XIX"
+		want := 19
 
-	// 	// Act
-	// 	got := romanToInt(input)
+		// Act
+		got := romanToInt(input)
 
-	// 	// Assert
-	// 	if got != want {
-	// 		t.Errorf("want %v , but got %v", want, got)
-	// 	}
-	// })
+		// Assert
+		if got != want {
+			t.Errorf("want %v , but got %v", want, got)
+		}
+	})
 
 	// t.Run("should return 30 when input is XXX", func(t *testing.T) {
 	// 	// Arrange
