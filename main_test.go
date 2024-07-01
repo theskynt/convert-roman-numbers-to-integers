6ccae73b27b9f3540661050c1545e05e43d5c95b
Narunart Tuntue <narunart.t@arise.tech>
2024-07-02 00:55:21 +0700

    [3] should return 40 when input is XL

--- main_test.go
+++ main_test.go
@@ -131,19 +131,19 @@ func TestRomanToInt(t *testing.T) {
 		}
 	})
 
-	// t.Run("should return 40 when input is XL", func(t *testing.T) {
-	// 	// Arrange
-	// 	input := "XL"
-	// 	want := 40
+	t.Run("should return 40 when input is XL", func(t *testing.T) {
+		// Arrange
+		input := "XL"
+		want := 40
 
-	// 	// Act
-	// 	got := romanToInt(input)
+		// Act
+		got := romanToInt(input)
 
-	// 	// Assert
-	// 	if got != want {
-	// 		t.Errorf("want %v , but got %v", want, got)
-	// 	}
-	// })
+		// Assert
+		if got != want {
+			t.Errorf("want %v , but got %v", want, got)
+		}
+	})
 
 	// t.Run("should return 50 when input is L", func(t *testing.T) {
 	// 	// Arrange
