338741ed76c3b9affea7493833032275c4dafd50
Narunart Tuntue <narunart.t@arise.tech>
2024-07-02 20:18:42 +0700

    [1] should return 1 when input is I

---
+++ V2/main_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestRomanToInt(t *testing.T) {
+	tests := []struct {
+		roman   string
+		want    int
+	}{
+		{"I", 1},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.roman, func(t *testing.T) {
+			got := romanToInt(tt.roman)
+			if got != tt.want {
+				t.Errorf("want %v , but got %v", tt.want, got)
+			}
+		})
+	}
+}
