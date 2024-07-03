80ae6dc50feb500ee4543e77dde5146bc78eadfe
Narunart Tuntue <narunart.t@arise.tech>
2024-07-03 20:32:35 +0700

    [3] should return 1 when input is I

--- V2/main_test.go
+++ V2/main_test.go
@@ -10,16 +10,6 @@ func TestRomanToInt(t *testing.T) {
 		want    int
 	}{
 		{"I", 1},
-		{"III", 3},
-		{"IV", 4},
-		{"V", 5},
-		{"X", 10},
-		{"XV", 15},
-		{"XXX", 30},
-		{"L", 50},
-		{"LXXX", 80},
-		{"XC", 90},
-		{"C", 100},
 	}
 
 	for _, tt := range tests {
